db481aa306b1e8d26c58c2b6feba8986a5b6fd5b
Hellboy-Aaryan <72555917+Hellboy-Aaryan@users.noreply.github.com>
2021-05-10 07:24:39 +0530

    Updater added
    Update via cmd Added!

--- src/main/java/com/example/updater.java
+++ src/main/java/com/example/updater.java
@@ -2,21 +2,24 @@ package com.example;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Repository;
+import org.apache.commons.io.FileUtils;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class updater extends botmaker {
     public void isUpdate(Update update) {
-        String msg = update.getMessage().toString();
+        String msg = update.getMessage().getText().toString();
         if (msg.equalsIgnoreCase("/update")) {
+            System.out.println("Updating the bot\nPlease Wait!");
             runmsg(update.getMessage().getChatId().toString(), "Updating Bot Maker...");
             try {
-                cloneRepository();
+                pull();
             } catch (GitAPIException | IOException e) {
                 runmsg(update.getMessage().getChatId().toString(), "Cannot Update Bot!");
                 e.printStackTrace();
@@ -26,20 +29,45 @@ public class updater extends botmaker {
         }
     }
 
-    public void cloneRepository() throws InvalidRemoteException, TransportException, GitAPIException, IOException {
-        Git.cloneRepository().setURI("https://github.com/Hellboy-Aaryan/EvilX.git")
-                .setDirectory(new File("E:/Telegram_Bot_JAVA/botmaker")).setCloneAllBranches(true).call();
-    }
+    public void pull() throws IOException, GitAPIException {
+        final File localPath;
+        try (Repository repository = cloneRepository()) {
+            localPath = repository.getWorkTree();
 
-    public static void createLocalRepo() throws IOException, IllegalStateException, GitAPIException {
-        File localPath = File.createTempFile("Testing", "");
-        Files.delete(localPath.toPath());
-        try (Git git = Git.init().setDirectory(localPath).call()) {
-            System.out.println("Created repository: " + git.getRepository().getDirectory());
-            File myFile = new File(git.getRepository().getDirectory().getParent(), "testfile");
-            if (!myFile.createNewFile()) {
-                throw new IOException("Could not create file " + myFile);
+            System.out.println("Having repository: " + repository.getDirectory() + " with head: "
+                    + repository.findRef(Constants.HEAD) + "/" + repository.resolve("HEAD") + "/"
+                    + repository.resolve("refs/heads/master"));
+
+            try (Git git = new Git(repository)) {
+                PullResult call = git.pull().call();
+
+                System.out.println("Pulled from the remote repository: " + call);
             }
         }
+
+        // clean up here to not keep using more and more disk-space for these samples
+        // FileUtils.deleteDirectory(localPath);
+    }
+
+    private static Repository cloneRepository() throws IOException, GitAPIException {
+        String REMOTE_URL = "https://github.com/Hellboy-Aaryan/TestUpdater.git";
+        // prepare a new folder for the cloned repository
+        File localPath = File.createTempFile("TestGitRepository", "", new File("E:/Telegram_Bot_JAVA/"));
+        if (!localPath.delete()) {
+            throw new IOException("Could not delete temporary file " + localPath);
+        }
+
+        // then clone
+        System.out.println("Cloning from " + REMOTE_URL + " to " + localPath);
+        try (Git result = Git.cloneRepository().setURI(REMOTE_URL).setDirectory(localPath).call()) {
+            // Note: the call() returns an opened repository already which needs to be
+            // closed to avoid file handle leaks!
+            return result.getRepository();
+        }
+    }
+
+    public void clonerepository() throws InvalidRemoteException, TransportException, GitAPIException, IOException {
+        Git.cloneRepository().setURI("https://github.com/Hellboy-Aaryan/TestUpdater.git")
+                .setDirectory(new File("E:/Telegram_Bot_JAVA/botmaker")).setCloneAllBranches(true).call();
     }
 }
